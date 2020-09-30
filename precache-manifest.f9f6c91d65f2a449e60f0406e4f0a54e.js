53df0b277fc719b6caa41152bcc4c7aa4653b64f
HeIU <minhhieu092@gmail.com>
2020-10-01 03:16:28 +0700

    Updates

---
+++ precache-manifest.f9f6c91d65f2a449e60f0406e4f0a54e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abd32eae5c10f646437cf89c3a2dc4b",
+    "url": "/minhhieu092.github.io/test-app/index.html"
+  },
+  {
+    "revision": "dae448b426402fbe90f0",
+    "url": "/minhhieu092.github.io/test-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f1ed1c2cb2f95e503fa2",
+    "url": "/minhhieu092.github.io/test-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dae448b426402fbe90f0",
+    "url": "/minhhieu092.github.io/test-app/static/js/2.43505d6b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/minhhieu092.github.io/test-app/static/js/2.43505d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ed1c2cb2f95e503fa2",
+    "url": "/minhhieu092.github.io/test-app/static/js/main.975d9bf3.chunk.js"
+  },
+  {
+    "revision": "adf22a00f6d31544d4b4",
+    "url": "/minhhieu092.github.io/test-app/static/js/runtime-main.07563cd8.js"
+  }
+]);
